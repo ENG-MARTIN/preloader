e589ceb05e5a41316354c7f1cc6936dacb50d2d5
ChipcodeDev <chipcodedev@Martin.www.chipcode.com>
2024-06-20 06:07:20 -0500

    preloader made

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 5000); // 5000 milliseconds = 5 seconds
+});
